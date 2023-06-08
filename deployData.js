f78c55d0e6a0b0ec95270885762b0dead06950f3
Andreas_upo <upoaranda19@gmail.com>
2023-06-08 17:22:22 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Andreas Bagus Upo Aranda'; // TODO: replace this
+
+const studentId = 'FE5597787'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://andreas-fe5597787.netlify.app';// TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
